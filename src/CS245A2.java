4c1c7e787b5120458cb93f3ff48c30b504cee1b2
lihanglin95 <37127371+lihanglin95@users.noreply.github.com>
2019-12-08 07:20:21 -0800

    3

--- src/CS245A2.java
+++ src/CS245A2.java
@@ -1,7 +1,6 @@
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -10,6 +9,10 @@ import java.util.LinkedList;
 public class CS245A2 {
     Graph graph = new Graph();
 
+    /**
+     * Main
+     * @param args This is the path of the source file
+     */
     public static void main(String[] args) throws Exception {
         CS245A2 test = new CS245A2();
         String filePath = args[0];
@@ -17,44 +20,50 @@ public class CS245A2 {
         test.readInput();
     }
 
+    /**
+     * This method is to read the source file and import the names to the graph
+     * @param filePath This is the path of the source file
+     */
     public void readFile(String filePath) throws Exception {
-        try(BufferedReader br = new BufferedReader(new FileReader("src/" + filePath))){
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
             br.readLine();
             String movie;
             while((movie = br.readLine()) != null){
-                movie = formatDoubleQuotes(split(movie));
+                movie = formatDoubleQuotes(castSplit(movie));
                 if(movie == null){
                     continue;
                 }
 
+                //JSON parser
                 JSONArray jarray = (JSONArray) new JSONParser().parse(movie);
                 ArrayList<String> names = new ArrayList<String>();
                 for(Object obj : jarray){
                     JSONObject jo = (JSONObject) obj;
-                    names.add((String) jo.get("name"));
+                    String nameStr = (String) jo.get("name");
+                    names.add(nameStr.toLowerCase());
                 }
 
-                Object[] obj = names.toArray();
-                for(Object a : obj){
-                    System.out.print(a);
-                }
-                System.out.println();
+                String[] array = names.toArray(new String[names.size()]);
+                graph.addEdges(array);
             }
-
         }
     }
 
-    public String split(String movie) {
+    /**
+     * This method is to split the PSON format String from source file
+     * @param movie This is the whole line of one movie information
+     * @return This returns the string after split or <>null</> if there is not cast information
+     */
+    public String castSplit(String movie) {
         String[] cast = movie.split("\\[\\{\"\"c");
         if(cast[0].contains("[]")){
             return null;
         }
 
-
         if(cast[1].charAt(0) == 'a'){
             String afterSplit;
             if(cast[1].contains("[]")){
-                afterSplit = "[{\"\"c" + cast[1].substring(0, cast[1].length() - 4);
+                afterSplit = "[{\"\"c" + cast[1].substring(0, cast[1].length() - 4 );
             }else {
                 afterSplit = "[{\"\"c" + cast[1].substring(0, cast[1].length() - 3);
             }
@@ -63,6 +72,11 @@ public class CS245A2 {
         return null;
     }
 
+    /**
+     * This method is to reformat the Pson format String
+     * @param input This is the unreadable Pson format String
+     * @return This returns the readable Pson format String
+     */
     public String formatDoubleQuotes(String input){
         StringBuilder sb = new StringBuilder();
         if(input == null){
@@ -86,6 +100,9 @@ public class CS245A2 {
         return sb.toString();
     }
 
+    /**
+     * This method is to read user input from console
+     */
     private void readInput() {
         boolean done = false;
 
@@ -93,30 +110,33 @@ public class CS245A2 {
             try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
                 System.out.println("Actor 1 name: ");
                 String actor1 = br.readLine().toLowerCase();
-                if(!graph.in_Graph(actor1)){
-                    System.out.println("\nNo such actor.\n");
-                    break;
+                while(!graph.in_Graph(actor1)) {
+                    System.out.println("No such actor.");
+                    System.out.print("Actor 1 name: ");
+                    actor1 = br.readLine().toLowerCase();
                 }
 
                 System.out.println("Actor 2 name: ");
                 String actor2 = br.readLine().toLowerCase();
-                if(!graph.in_Graph(actor2)){
-                    System.out.println("\nNo such actor.\n");
-                    break;
+                while(!graph.in_Graph(actor2)) {
+                    System.out.println("No such actor.");
+                    System.out.print("Actor 2 name: ");
+                    actor2 = br.readLine().toLowerCase();
                 }
 
                 LinkedList<String> result = graph.findShortestPath(actor2, actor1);
                 if(result != null){
-                    System.out.printf("Path between %s and %s: ", actor1, actor2);
+                    System.out.printf("Path between %s and %s: ", formatName(actor1), formatName(actor2));
                     print(result);
                 }else{
-                    System.out.println("There is not path from " + actor1 + " to " + actor2 + ".");
+                    System.out.println("There is not path from " + formatName(actor1) + " to " + formatName(actor2) + ".");
                 }
 
                 System.out.println("Continue? [Y/N]\n");
                 String finished = br.readLine();
                 if(finished.toLowerCase().charAt(0) == 'y'){
                     done = false;
+                    readInput();
                 }else{
                     done = true;
                 }
@@ -126,11 +146,28 @@ public class CS245A2 {
         }
     }
 
+    /**
+     * This method is to reformat the actors' names
+     * @param name This is the actor's name
+     * @return This returns the actor's name
+     */
+    public String formatName(String name) {
+        String[] names = name.split(" ");
+        String result = "";
+        for(String part: names)
+            result += " "+part.substring(0,1).toUpperCase()+part.substring(1);
+        return result.substring(1);
+    }
+
+    /**
+     * This method is to print the path
+     * @param result This is the list of actors in the shortest path
+     */
     public void print(LinkedList<String> result) {
 
         for(int i = 0; i < result.size() - 1; i++){
-            System.out.printf("%s --> ", result.get(i));
+            System.out.printf("%s --> ", formatName(result.get(i)));
         }
-        System.out.println(result.get(result.size() - 1));
+        System.out.println(formatName(result.get(result.size() - 1)));
     }
 }
--- src/Graph.java
+++ src/Graph.java
@@ -3,16 +3,21 @@ import java.util.*;
 public class Graph {
     HashMap<String, LinkedList<String>> adjGraph;
 
+    /*
+    Constructor
+     */
     public Graph(){
         adjGraph = new HashMap<String, LinkedList<String>>();
     }
 
+    /**
+     * This method is to add edges between the actors in the same movie
+     * @param actors This is the array of actors in the same movie
+     */
     public void addEdges(String[] actors){
         for(String actor : actors){
-            actor = actor.toLowerCase();
             if(adjGraph.containsKey(actor)){
                 for(String subActor : actors){
-                    subActor = subActor.toLowerCase();
                     if(!subActor.equals(actor) || !adjGraph.get(actor).contains(subActor)){
                         adjGraph.get(actor).add(subActor);
                     }
@@ -24,6 +29,12 @@ public class Graph {
         }
     }
 
+    /**
+     * This method is to find the shortest path between two target actors
+     * @param from This is actor2
+     * @param to This is actor1
+     * @return This returns the list of actors in the shortest path
+     */
     public LinkedList<String> findShortestPath(String from, String to){
         HashMap<String, String> visited = new HashMap<String, String>(adjGraph.size());
         LinkedList<String> queue = new LinkedList<String>();
@@ -41,20 +52,34 @@ public class Graph {
                 }
                 result.add(current);
                 return result;
+            }else{
+                ListIterator<String> it = adjGraph.get(current).listIterator();
+                while(it.hasNext()){
+                    String visitingActor = it.next();
+                    if(!visited.containsKey(visitingActor)){
+                        visited.put(visitingActor, current);
+                        queue.add(visitingActor);
+                    }
+                }
+
+//                for(String name : adjGraph.get(current)){
+//                    if(!visited.containsKey(name)){
+//                        visited.put(name, current);
+//                        queue.add(name);
+//                    }
+//                }
             }
 
-            Iterator<String> it = adjGraph.get(current).listIterator();
-            while(it.hasNext()){
-                String visitingActor = it.next();
-                if(!visited.containsKey(visitingActor)){
-                    visited.put(visitingActor, current);
-                    queue.add(visitingActor);
-                }
-            }
+
         }
-        return null;
+        return result;
     }
 
+    /**
+     * This method is to check if the actor name is in the graph
+     * @param actor This is the actor's name
+     * @return This returns <>true</> if it contains or <>false</> otherwise
+     */
     public boolean in_Graph(String actor){
         return adjGraph.containsKey(actor);
     }
